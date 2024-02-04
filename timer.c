2d97ea3d67379dfe3d5f258cac3845d3f421df5c
MaxAlberti <maxalberti@gmx.de>
2024-02-04 19:30:39 +0100

    Added advanced timer functionality

--- timer.c
+++ timer.c
@@ -5,73 +5,204 @@
 
 #include "register_access.h"
 
-void timer_init_detailed(uint32_t prescaler, uint32_t bitmode,
-                         uint32_t compareValue) {
+void timer_init(enum Timer timer, uint32_t prescaler, uint32_t bitmode, uint32_t cc0) {
+  // Add prescaler
+  timer_prescaler(timer, prescaler);
+  // Add bitmode
+  timer_bitmode(timer, bitmode);
+  // Add capture target
+  timer_add_capture(timer, CC0, cc0);
+  timer_remove_capture(timer, CC1);
+  timer_remove_capture(timer, CC2);
+  timer_remove_capture(timer, CC3);
+}
 
-  // Instance[0] of the Timer uses Peripheral ID: 8
+void timer_prescaler(enum Timer timer, uint32_t prescaler) {
+  /* Prescaler --------------------------------------------------------------
+  Set the Prescaler to min (1 = 0b00000001)
+  register_write((TIMER0_BASE_ADDRESS + TIMER_PRESCALER), 1);
 
-  // Prescaler --------------------------------------------------------------
-  // Set the Prescaler to min (1 = 0b00000001)
-  // register_write((TIMER0_BASE_ADDRESS + TIMER_PRESCALER), 1);
+  Set the Prescaler to max (15 = 0b00001111)
+  register_write((TIMER0_BASE_ADDRESS + TIMER_PRESCALER), 15);
 
-  // Set the Prescaler to max (15 = 0b00001111)
-  // register_write((TIMER0_BASE_ADDRESS + TIMER_PRESCALER), 15);
+  Set the Prescaler to custom value from arguments
+  */
+  register_write((timer + TIMER_PRESCALER), prescaler);
+}
 
-  // Set the Prescaler to custom value from arguments
-  register_write((TIMER0_BASE_ADDRESS + TIMER_PRESCALER), prescaler);
+void timer_bitmode(enum Timer timer, uint32_t bitmode) {
+  /* BitMode ----------------------------------------------------------------
+  set BitMode Register = use 8bit
+  register_write((TIMER0_BASE_ADDRESS + TIMER_BITMODE), 0);
 
-  // BitMode ----------------------------------------------------------------
-  // set BitMode Register = use 8bit
-  // register_write((TIMER0_BASE_ADDRESS + TIMER_BITMODE), 0);
+  set BitMode Register = use 32bit
+  register_write((TIMER0_BASE_ADDRESS + TIMER_BITMODE), 3);
 
-  // set BitMode Register = use 32bit
-  // register_write((TIMER0_BASE_ADDRESS + TIMER_BITMODE), 3);
+  set BitMode Register to custom value from arguments 
+  */
+  register_write((timer + TIMER_BITMODE), bitmode);
+}
 
-  // set BitMode Register to custom value from arguments
-  register_write((TIMER0_BASE_ADDRESS + TIMER_BITMODE), bitmode);
+void timer_add_capture(enum Timer timer, enum Capture cc, uint32_t compareValue) {
+  uint32_t timer_cc;
+  uint32_t int_compare;
+  uint32_t clear;
+  switch (cc) {
+    case CC0:
+      timer_cc = TIMER_CC_0;
+      int_compare = INT_COMPARE0; // Interrupt on Compare[0]
+      clear = 0x01; //  A=1 -- CLEAR on CC[0]
+      break;
+    case CC1:
+      timer_cc = TIMER_CC_1;
+      int_compare = INT_COMPARE1;
+      clear = 0x02; //  B=1 -- CLEAR on CC[1]
+      break;
+    case CC2:
+      timer_cc = TIMER_CC_2;
+      int_compare = INT_COMPARE2;
+      clear = 0x04; //  C=1 -- CLEAR on CC[2]
+      break;
+    case CC3:
+      timer_cc = TIMER_CC_3;
+      int_compare = INT_COMPARE3;
+      clear = 0x08; //  D=1 -- CLEAR on CC[3]
+      break;
+    default:
+      timer_cc = TIMER_CC_0;
+      int_compare = INT_COMPARE0;
+      clear = 0x01; //  A=1 -- CLEAR on CC[0]
+      break;
+  }
 
-  // Compare Value ----------------------------------------------------------
-  // Compare Value to 255
-  // register_write((TIMER0_BASE_ADDRESS + TIMER_CC_0), 255); // fast as hell!
+  /* Compare Value ----------------------------------------------------------
+   Compare Value to 255
+   register_write((TIMER0_BASE_ADDRESS + TIMER_CC_0), 255); // fast as hell!
 
-  // Compare/Count to ~4sek
-  // register_write((TIMER0_BASE_ADDRESS + TIMER_CC_0), 1953); // ~4 sek
+   Compare/Count to ~4sek
+   register_write((TIMER0_BASE_ADDRESS + TIMER_CC_0), 1953); // ~4 sek
 
-  // Compare/Count to custom value from arguments
-  register_write((TIMER0_BASE_ADDRESS + TIMER_CC_0), compareValue); // ~4 sek
+   Compare/Count to custom value from arguments
+  */
+  register_write((timer + timer_cc), compareValue);
 
-  // Optional: Shortcuts ----------------------------------------------------
-  // Enable Shortcut between CC[0] and STOP + CLEAR
-  // register_write((TIMER0_BASE_ADDRESS + TIMER_SHORTS), 0x101); // G=1 + A=1
-
-  // Enable Shortcut between CC[0] and CLEAR
-  register_write((TIMER0_BASE_ADDRESS + TIMER_SHORTS),
-                 0x01); //  A=1 -- CLEAR on CC[0]
-
-  // Start the Module -------------------------------------------------------
+  // Enable Shortcuts ----------------------------------------------------
+  // - Enable Shortcut between CC[0] and STOP + CLEAR
+  // register_write((timer + TIMER_SHORTS), 0x101); // G=1 + A=1
+  // - Enable Shortcut between CC[0] and CLEAR
+  uint32_t shorts = register_read((timer + TIMER_SHORTS));
+  register_write((timer + TIMER_SHORTS), clear | shorts); // Merge with previous
 
   // Enable Interrupt
-  register_write((TIMER0_BASE_ADDRESS + TIMER_INTENSET),
-                 INT_COMPARE0); // Interrupt on Compare[0]
+  uint32_t ints = register_read((timer + TIMER_INTENSET));
+  register_write((timer + TIMER_INTENSET), int_compare | ints); // Merge with previous
+}
 
+void timer_remove_capture(enum Timer timer, enum Capture cc) {
+  uint32_t timer_cc;
+  uint32_t int_compare;
+  uint32_t clear;
+  switch (cc) {
+    case CC0:
+      timer_cc = TIMER_CC_0;
+      int_compare = INT_COMPARE0; // Interrupt on Compare[0]
+      clear = 0x01; //  A=1 -- CLEAR on CC[0]
+      break;
+    case CC1:
+      timer_cc = TIMER_CC_1;
+      int_compare = INT_COMPARE1;
+      clear = 0x02; //  B=1 -- CLEAR on CC[1]
+      break;
+    case CC2:
+      timer_cc = TIMER_CC_2;
+      int_compare = INT_COMPARE2;
+      clear = 0x04; //  C=1 -- CLEAR on CC[2]
+      break;
+    case CC3:
+      timer_cc = TIMER_CC_3;
+      int_compare = INT_COMPARE3;
+      clear = 0x08; //  D=1 -- CLEAR on CC[3]
+      break;
+    default:
+      timer_cc = TIMER_CC_0;
+      int_compare = INT_COMPARE0;
+      clear = 0x01; //  A=1 -- CLEAR on CC[0]
+      break;
+  }
+
+  // Disable Shortcuts ----------------------------------------------------
+  // - Disable Shortcut between CC[0] and STOP + CLEAR
+  // register_write((timer + TIMER_SHORTS), 0x101); // G=1 + A=1
+  // - Disable Shortcut between CC[0] and CLEAR
+  uint32_t shorts = register_read((timer + TIMER_SHORTS));
+  register_write((timer + TIMER_SHORTS), (~clear) & shorts); // Overwrite previous
+
+  // Disable Interrupt
+  uint32_t ints = register_read((timer + TIMER_INTENSET));
+  register_write((timer + TIMER_INTENSET), (~int_compare) & ints); // Overwrite previous
+}
+
+void timer_start(enum Timer timer) {
+  uint32_t iid;
+  switch (timer)
+  {
+  case TIMER0:
+    iid = Interrupt_ID8;
+    break;
+  case TIMER1:
+    iid = Interrupt_ID9;
+    break;
+  case TIMER2:
+    iid = Interrupt_ID10;
+    break;
+  default:
+    iid = Interrupt_ID8;
+    break;
+  }
+
+  // Start the Module -------------------------------------------------------
   // Start Timer
-  register_write((TIMER0_BASE_ADDRESS + TIMER_START), TIMER_TASK_START);
+  register_write((timer + TIMER_START), TIMER_TASK_START);
 
   // Enable User-Interrupt from Cortex-M0
   // ID8 ist der Timer0
-  register_write(Interrupt_Set_Enable, Interrupt_ID8);
+  uint32_t ints = register_read(Interrupt_Set_Enable);
+  register_write(Interrupt_Set_Enable, iid | ints); // Merge with existing
 }
 
-void timer_init() {
+void timer_stop(enum Timer timer) {
+  uint32_t iid;
+  switch (timer)
+  {
+  case TIMER0:
+    iid = Interrupt_ID8;
+    break;
+  case TIMER1:
+    iid = Interrupt_ID9;
+    break;
+  case TIMER2:
+    iid = Interrupt_ID10;
+    break;
+  default:
+    iid = Interrupt_ID8;
+    break;
+  }
 
-  // Init Timer with an interval of ~4sek
-  timer_init_detailed(15, 3, 1953);
+  // Start the Module -------------------------------------------------------
+  // Start Timer
+  register_write((timer + TIMER_STOP), TIMER_TASK_START);
+
+  // Enable User-Interrupt from Cortex-M0
+  // ID8 ist der Timer0
+  uint32_t ints = register_read(Interrupt_Set_Enable);
+  register_write(Interrupt_Set_Enable, (~iid) & ints); // Overwrite existing
 }
 
-void timer_clearCompareEvent() {
-
+void timer_clear(enum Timer timer, enum Capture cc) {
   // Write a `0` to the compare 0 Register.
-  // This is sufficient, because this example only uses CC[0]
-  // If you're using more, feel free to expand.
-  register_write((TIMER0_BASE_ADDRESS + TIMER_COMPARE_0), TIMER_EVENT_CLEAR);
+  register_write((timer + cc), TIMER_EVENT_CLEAR);
+}
+
+uint32_t timer_check(enum Timer timer, enum Capture cc) {
+  return register_read((timer + cc));
 }
--- timer.h
+++ timer.h
@@ -69,34 +69,100 @@
 #define INT_COMPARE2 (1 << 18) // Enable/Disable Interrupt on COMPARE[2] event
 #define INT_COMPARE3 (1 << 19) // Enable/Disable Interrupt on COMPARE[3] event
 
+// Custom
+
+/**
+ * @brief Enum of all available timers.
+ * Timer0, Timer1, Timer2
+ */
+enum Timer {
+  TIMER0=TIMER0_BASE_ADDRESS,
+  TIMER1=TIMER1_BASE_ADDRESS,
+  TIMER2=TIMER2_BASE_ADDRESS,
+};
+
+enum Capture {
+  CC0=TIMER_COMPARE_0,
+  CC1=TIMER_COMPARE_1,
+  CC2=TIMER_COMPARE_2,
+  CC3=TIMER_COMPARE_3,
+};
+
+
 // C Function Definitions -----------------------------------------------------
 
 /**
- * @brief Initializes the Timer[0] Peripheral to a value ~4sek
- * This is just for demonstration and could be changed to your needs
+ * @brief Initialize a new timer with one capture target. Does NOT start the timer.
+ * timer_init(TIMER0, 15, 3, 1953); initializes Timer0 to tick on cc0 every ~4sec.
+ * @param timer Timer to use. Timer0..2
+ * @param prescaler Prescaler to reduce percision. min (1 = 0b00000001), max (15 = 0b00001111)
+ * @param bitmode Timer bitmode. 0=16b, 1=8b, 2=24b, 3=32b
+ * @param cc0 Capture Target 0. Value to compare timer against.
  */
-void timer_init();
+void timer_init(enum Timer timer, uint32_t prescaler, uint32_t bitmode, uint32_t cc0);
 
 /**
- * @brief Initializes the Timer[0] Peripheral to the Values given
- * It also enable the shortcut between CC[0] and CLEAR, so that the timer
- * is cleared, when the compared value is reached.
- *
- * Compare Value set is for CC[0]
- *
- * @param prescaler    Register value according to `Table 147: PRESCALER`
- * @param bitmode      Register value according to `Table 146: BITMODE`
- * @param compareValue Register value according to `Table 148: CC[0]`
+ * @brief Changes the timer prescaler.
+ * 
+ * @param timer Selected timer
+ * @param prescaler Prescaler to reduce percision. min (1 = 0b00000001), max (15 = 0b00001111)
  */
-void timer_init_detailed(uint32_t prescaler, uint32_t bitmode,
-                         uint32_t compareValue);
+void timer_prescaler(enum Timer timer, uint32_t prescaler);
 
 /**
- * @brief Clears the Compare Event for CC[0]
- *
- * This could be useful for stopping the
- * timer from pulling the interrupt-line.
+ * @brief Change timer bitmode
+ * 
+ * @param timer Selected timer
+ * @param bitmode Timer bitmode. 0=16b, 1=8b, 2=24b, 3=32b
  */
-void timer_clearCompareEvent();
+void timer_bitmode(enum Timer timer, uint32_t bitmode);
+
+/**
+ * @brief Add timer capture target
+ * 
+ * @param timer Selected timer
+ * @param cc Selected capture group
+ * @param compareValue Value to compare timer against
+ */
+void timer_add_capture(enum Timer timer, enum Capture cc, uint32_t compareValue);
+
+/**
+ * @brief Remove timer capture target
+ * 
+ * @param timer Selected timer
+ * @param cc Selected capture group
+ */
+void timer_remove_capture(enum Timer timer, enum Capture cc);
+
+/**
+ * @brief Start timer
+ * 
+ * @param timer Selected timer
+ */
+void timer_start(enum Timer timer);
+
+/**
+ * @brief Remove timer
+ * 
+ * @param timer Selected timer
+ */
+void timer_stop(enum Timer timer);
+
+/**
+ * @brief Clears the Compare Event
+ * 
+ * @param timer Selected timer
+ * @param cc Selected capture group
+ */
+void timer_clear(enum Timer timer, enum Capture cc);
+
+/**
+ * @brief Check if timer event has triggered
+ * 
+ * @param timer Selected timer
+ * @param cc Selected capture group
+ * @return uint32_t 
+ */
+uint32_t timer_check(enum Timer timer, enum Capture cc);
 
 #endif
