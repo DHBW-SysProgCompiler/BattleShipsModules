88edb7d1d9f869c2f36436abc7f7ffc5854fbcd6
Erix <e.a.erath@gmail.com>
2024-02-04 18:02:38 +0100

    implementad basic functions

---
+++ screen_control.c
@@ -0,0 +1,10 @@
+#include "uart.h"
+
+void init() { uart_init(); }
+void printc(char c) { uart_writeByte(c); }
+void print(char s[], uint32_t len) {
+  for (int i = 0; i < len; i++) {
+    printc(s[i]);
+  }
+}
+void clear_screen() { print("\e[2J", 4); }
---
+++ screen_control.h
@@ -0,0 +1,9 @@
+#ifndef SCREENCONTROL_H
+#define SCREENCONTROL_H
+
+void init();
+void clear_screen();
+void printc(char);
+void print(char[], uint32_t);
+
+#endif
