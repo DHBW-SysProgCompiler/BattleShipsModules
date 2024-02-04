6571223686512adc128a8deb4e730adf04251cb3
Erix <e.a.erath@gmail.com>
2024-02-04 18:16:44 +0100

    added documentation

--- screen_control.c
+++ screen_control.c
@@ -1,12 +1,26 @@
-#include <stdint.h>
-#include "uart.h"
 #include "screen_control.h"
+#include "uart.h"
+#include <stdint.h>
 
-void init() { uart_init(); }
-void printc(char c) { uart_writeByte(c); }
+void init() {
+  // initialize uart
+  uart_init();
+  // nothing else to be done *yet*
+}
+
+void printc(char c) {
+  // simply pass char to uart
+  uart_writeByte(c);
+}
+
 void print(char s[], uint32_t len) {
+  // iterate over s, print each char individually
   for (int i = 0; i < len; i++) {
     printc(s[i]);
   }
 }
-void clear_screen() { print("\e[2J", 4); }
+
+void clear_screen() {
+  // clears the screen via ansi-voodoo
+  print("\e[2J", 4);
+}
--- screen_control.h
+++ screen_control.h
@@ -3,9 +3,29 @@
 
 #include <stdint.h>
 
+/**
+ * @brief Initialize the Screen-Controller
+ */
 void init();
+
+/**
+ * @brief clear the console-screen
+ */
 void clear_screen();
-void printc(char);
-void print(char[], uint32_t);
+
+/**
+ * @brief Print a singe char
+ *
+ * @param c char to be printed
+ */
+void printc(char c);
+
+/**
+ * @brief Prints an entire String (char-array)
+ *
+ * @param s the char-array to be printed
+ * @param len length of the array
+ */
+void print(char s[], uint32_t len);
 
 #endif
